a226f1d5108a4fd0ab5a84282fc45c0572261ea9
denzel <d.x.trager@student.tudelft.nl>
2024-11-24 23:13:07 +0100

    Put __main__ in a function.

--- src/__main__.py
+++ src/__main__.py
@@ -1,15 +1,21 @@
 import sys
 
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QWidget
 
 from src.image_select import MainWindow
 
-if __name__ == "__main__":
+
+def create_app() -> tuple[QApplication, QWidget]:
     app = QApplication(sys.argv)
     myApp = MainWindow()
     appIcon = QIcon()
     appIcon.addFile("..\\crt_icon.ico")
     myApp.setWindowIcon(appIcon)
     myApp.show()
-    app.exec()
+    return app, myApp
+
+
+if __name__ == "__main__":
+    app, _ = create_app()
+    sys.exit(app.exec())
